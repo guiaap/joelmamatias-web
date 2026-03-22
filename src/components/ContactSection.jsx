f303388a7bb31955c1f0d2a7826cf762ad180c87
Guilherme Amaral <guilhermeamaral325@gmail.com>
2026-03-22 19:08:11 -0300

    refactor: melhora a indentação/organização do código da seção contato

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -4,93 +4,223 @@ import Icon from "./Icon.jsx";
 
 import instagram from "../assets/images/instagram.png";
 
+export function FormInput({
+    as = "input",
+    type = "text", 
+    label, 
+    name, 
+    placeholder, 
+    isRequired = false,
+    value,
+    ...rest
+}) {
+    if(as === "textarea") {
+
+        return (
+
+            <div className="flex flex-col gap-2 mb-6">
+
+                <label
+                    htmlFor={name}
+                    className="
+                        text-(--transparent-white-smooth)
+                        text-[0.8rem]
+                        uppercase tracking-widest
+                ">
+                    {label}
+                </label>
+
+                <textarea
+                    name={name}
+                    placeholder={placeholder}
+                    required={isRequired}
+                    className="
+                        resize-none h-40
+                        p-3
+                        text-(--white)
+                        bg-[#FFFFFF10]
+                        border-2 border-[#FFFFFF20]
+                        transition-all duration-50
+
+                        placeholder:text-[#FFFFFF70]
+                        placeholder:text-[0.9rem]
+                        placeholder:font-light
+
+                        focus:outline-none
+                        focus:border-2 focus:border-(--primary-gold)
+                    "
+                    {...rest}
+                />
+
+            </div>
+        )
+    }
+
+    return (
+
+        <div className="flex flex-col gap-2 mb-6">
+
+            <label 
+                htmlFor={name}
+                className="
+                    text-(--transparent-white-smooth)
+                    text-[0.8rem]
+                    uppercase tracking-widest
+            ">
+                {label}
+            </label>
+
+            <input
+                type={type}
+                id={name}
+                name={name}
+                placeholder={placeholder}
+                required={isRequired}
+                value={value}
+                className={`
+                    p-3
+                    text-[0.9rem]
+                    transition-all duration-300
+
+                    ${type === "submit" ? `
+                        block w-full
+                        uppercase tracking-widest
+                        bg-(--primary-gold)
+                        cursor-pointer
+                        hover:bg-(--secondary-gold)
+                        hover:-translate-y-1.25
+                    ` 
+                    : `
+                        text-(--white)
+                        bg-[#FFFFFF10]
+                        border-2 border-[#FFFFFF20]
+
+                        placeholder:text-[0.9rem]
+                        placeholder:font-light
+                        placeholder:text-[#FFFFFF70]
+
+                        focus:outline-none
+                        focus:border-2 focus:border-(--primary-gold)
+                    `}   
+            `}/>
+
+        </div>
+    )
+}
+
 export function ContactForm() {
 
     return (
 
         <form 
             className="
-                flex-1 p-5 md:p-8
-                w-full max-w-[650px] 
+                flex-1 
+                p-5 pb-2 md:p-8 md:pb-3
+                w-full max-w-162.5
                 bg-[#FFFFFF05]
                 border border-[#FFFFFF20]
-
-                [&>div]:flex [&>div]:flex-col [&>div]:gap-2
-                [&>div]:mb-6
-
-                [&>div>label]:text-(--transparent-white-smooth)
-                [&>div>label]:text-[0.8rem]
-                [&>div>label]:tracking-widest
-                [&>div>label]:uppercase
-
-                [&>div>input,textarea]:border-2 
-                [&>div>input,textarea]:border-[#FFFFFF20]
-                [&>div>input,textarea]:bg-[#FFFFFF10]
-                [&>div>input,textarea]:text-(--white)
-                [&>div>input,textarea]:transition-all
-                [&>div>input,textarea]:duration-500
-                [&>div>input,textarea]:px-3 [&>div>input,textarea]:py-3
-                [&>div>input::placeholder,textarea::placeholder]:text-[#FFFFFF70]
-                [&>div>input::placeholder,textarea::placeholder]:text-[0.9rem]
-                [&>div>input::placeholder,textarea::placeholder]:font-light
-                [&>div>input:focus,textarea:focus]:outline-none
-                [&>div>input:focus,textarea:focus]:border-2
-                [&>div>input:focus,textarea:focus]:border-(--primary-gold)
         ">
 
-            <div>
-
-                <label htmlFor="name">Nome</label>
-
-                <input 
-                    id="name"
+                <FormInput 
+                    type="text"
+                    label="Nome"
                     name="name"
-                    type="text" 
                     placeholder="Seu nome completo"
-                    required 
+                    isRequired={true}
                 />
 
-            </div>
-
-            <div>
-                <label htmlFor="email">E-mail</label>
-
-                <input 
-                    id="email"
+                <FormInput 
+                    type="email"
+                    label="email"
                     name="email"
-                    type="email" 
-                    placeholder="Seu e-mail" 
-                    required
+                    placeholder="Seu e-mail"
+                    isRequired={true}
                 />
 
-            </div>
 
-            <div>
-                <label htmlFor="message">Mensagem</label>
-                <textarea 
-                    id="message"
+                <FormInput 
+                    as="textarea"
+                    label="Mensagem"
                     name="message"
-                    placeholder="Como posso ajudar?"
-                    className="resize-none h-[160px]"
+                    placeholder="Como posso te ajudar?"
                 />
-                
-            </div>
 
-            <input 
-                type="submit" 
-                value="Enviar Mensagem" 
-                className="
-                    block w-full py-3
-                    uppercase tracking-widest
-                    text-[0.9rem]
-                    bg-(--primary-gold)
-                    cursor-pointer
-                    transition-all duration-300
-                    hover:bg-(--secondary-gold)
-                    hover:-translate-y-1.25
-            "/>
+                <FormInput 
+                    type="submit"
+                    value="Enviar"
+                />
+
             </form>
     );
+}
+
+export function Separator() {
+
+    return (
+        <div className="h-px w-20 my-5 bg-(--transparent-white-strong)" />
+    )
+}
+
+export function SocialMediaLink() {
+
+    return (
+
+        <a  
+            href="https://www.instagram.com/jm.gestaointeligente/" 
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center gap-4 mb-8 w-fit
+        ">
+            <img 
+                src={instagram} 
+                alt="Logo do Instagram" 
+                className="w-8 h-8"
+            />
+
+            <span>
+                @jm.gestaointeligente
+            </span>
+        </a>
+
+    )
+}
+
+export function OtherContactsItem({iconName, text}) {
+
+    return (
+
+        <li className="
+            flex items-center gap-2
+            [&>svg]:fill-(--transparent-white-smooth)
+        ">  
+            <Icon name={iconName} size="22px" />
+            <span>{text}</span>
+        </li>
+    )
+}
+
+export function WhatsAppButton() {
+
+    return (
+
+        <a 
+            href="https://wa.me/47991181188" 
+            target="_blank"
+            rel="noopener noreferrer"
+            className="
+                block w-fit
+                uppercase tracking-widest
+                text-[0.8rem] text-(--green)
+                py-3 px-5
+                border border-(--transparent-green-smooth)
+                transition-colors duration-300
+                hover:bg-(--transparent-green-strong)
+                hover:border-(--green)
+        ">
+            Entrar em contato via WhatsApp
+        </a>
+        
+    )
 }
 
 export default function ContactSection() {
@@ -98,14 +228,20 @@ export default function ContactSection() {
     return (
 
         <section 
-            id="contact" c
+            id="contact"
             className="bg-(--primary-brown)
-            
         ">
 
-            <div className="flex flex-col lg:flex-row items-start justify-between gap-10 max-w-(--container) mx-auto">
+            <div className="
+                flex flex-col lg:flex-row 
+                items-start justify-between gap-10 
+                max-w-(--container) mx-auto
+            ">
 
-                <div className="text-(--transparent-white-smooth) max-w-[700px] flex-1">
+                <div className="
+                    max-w-175 flex-1
+                    text-(--transparent-white-smooth)
+                ">
 
                     <SectionTag text="Fale Comigo" />
 
@@ -119,65 +255,28 @@ export default function ContactSection() {
                         Preencha o formulário ou use um dos canais abaixo.
                     </p>
 
-                    <div className="h-[1px] w-[50px] my-8 bg-(--transparent-white-smooth)" />
+                   <Separator />
 
                     <h3 className="uppercase text-[0.8rem] tracking-widest mb-5">
                         Siga-me
                     </h3>
 
-                    <a 
-                        href="https://www.instagram.com/jm.gestaointeligente/" 
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="flex items-center gap-4 mb-8 w-fit
-                    ">
-                        <img 
-                            src={instagram} 
-                            alt="Logo do Instagram" 
-                            className="w-10 h-10"
-                        />
-
-                        <span>
-                            @jm.gestaointeligente
-                        </span>
-                    </a>
+                    <SocialMediaLink />
 
 
                     <h3 className="uppercase tracking-widest text-[0.8rem] mb-5">
                         Outras formas de contato
                     </h3>
 
-                    <ul className="
-                        flex flex-col gap-2 mb-8
-                        [&>li]:flex [&>li]:gap-2 [&>li]:items-center
-                        [&>li>svg]:fill-(--transparent-white-smooth)
-                    ">
-                        <li>
-                            <Icon name="mail" />
-                            <span>comercial@joelmamatias.com.br</span>
-                        </li>
-                        <li>
-                            <Icon name="phone" />
-                            <span>(47) 99118-1188</span>
-                        </li>
+                    <ul className="flex flex-col gap-2 mb-8">
+
+                        <OtherContactsItem iconName="mail" text="comercial@joelmamatias.com.br"  />
+                        <OtherContactsItem iconName="phone" text="(47) 99118-1188"  />
+                        
                     </ul>
 
-                    <a 
-                        href="https://wa.me/47991181188" 
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="
-                        block w-fit
-                        uppercase tracking-widest
-                        text-[0.8rem] text-(--green)
-                        py-3 px-5
-                        border border-(--transparent-green-smooth)
-                        transition-colors duration-300
-                        hover:bg-(--transparent-green-strong)
-                        hover:border-(--green)
-                    ">
-                        Entrar em contato via WhatsApp
-                    </a>
+                    <WhatsAppButton />
+
                 </div>
 
                <ContactForm />
